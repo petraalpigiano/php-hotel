9d57da45458d2e0345ef1f781189bc2ed6de7b38
petraalpigiano <petraalpigiano@gmail.com>
2025-07-29 11:22:08 +0200

    add table

--- index.php
+++ index.php
@@ -10,57 +10,118 @@
 </head>
 
 <body>
-  <h1>boh</h1>
-  <?php
-  $hotels = [
+  <div class="container">
+    <h1>Lista Hotel</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <?php
+          $hotels = [
 
-    [
-      'name' => 'Hotel Belvedere',
-      'description' => 'Hotel Belvedere Descrizione',
-      'parking' => true,
-      'vote' => 4,
-      'distance_to_center' => 10.4
-    ],
-    [
-      'name' => 'Hotel Futuro',
-      'description' => 'Hotel Futuro Descrizione',
-      'parking' => true,
-      'vote' => 2,
-      'distance_to_center' => 2
-    ],
-    [
-      'name' => 'Hotel Rivamare',
-      'description' => 'Hotel Rivamare Descrizione',
-      'parking' => false,
-      'vote' => 1,
-      'distance_to_center' => 1
-    ],
-    [
-      'name' => 'Hotel Bellavista',
-      'description' => 'Hotel Bellavista Descrizione',
-      'parking' => false,
-      'vote' => 5,
-      'distance_to_center' => 5.5
-    ],
-    [
-      'name' => 'Hotel Milano',
-      'description' => 'Hotel Milano Descrizione',
-      'parking' => true,
-      'vote' => 2,
-      'distance_to_center' => 50
-    ],
+            [
+              'name' => 'Hotel Belvedere',
+              'description' => 'Hotel Belvedere Descrizione',
+              'parking' => true,
+              'vote' => 4,
+              'distance_to_center' => 10.4
+            ],
+            [
+              'name' => 'Hotel Futuro',
+              'description' => 'Hotel Futuro Descrizione',
+              'parking' => true,
+              'vote' => 2,
+              'distance_to_center' => 2
+            ],
+            [
+              'name' => 'Hotel Rivamare',
+              'description' => 'Hotel Rivamare Descrizione',
+              'parking' => false,
+              'vote' => 1,
+              'distance_to_center' => 1
+            ],
+            [
+              'name' => 'Hotel Bellavista',
+              'description' => 'Hotel Bellavista Descrizione',
+              'parking' => false,
+              'vote' => 5,
+              'distance_to_center' => 5.5
+            ],
+            [
+              'name' => 'Hotel Milano',
+              'description' => 'Hotel Milano Descrizione',
+              'parking' => true,
+              'vote' => 2,
+              'distance_to_center' => 50
+            ],
 
-  ];
-  echo "<pre>";
-  var_dump($hotels);
-  echo "</pre>";
-  foreach ($hotels as $currentHotel) {
-    foreach ($currentHotel as $key => $value) {
-      echo "$key : $value";
-      echo "<br>";
-    }
-  }
-  ?>
+          ];
+          // echo "<pre>";
+          // var_dump($hotels);
+          // echo "</pre>";
+          for ($i = 0; $i < 1; $i++) {
+            foreach ($hotels[0] as $key => $value) {
+              echo "<th scope='col'>$key</th>";
+            }
+          }
+          ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $hotels = [
+
+          [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+          ],
+          [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+          ],
+          [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+          ],
+          [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+          ],
+          [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+          ],
+
+        ];
+        // echo "<pre>";
+        // var_dump($hotels);
+        // echo "</pre>";
+        foreach ($hotels as $currentHotel) {
+          echo "<tr></tr>";
+          foreach ($currentHotel as $key => $value) {
+            echo "<td>$value</th>";
+          }
+        }
+        ?>
+
+      </tbody>
+    </table>
+  </div>
+
   <!-- BOOTSTRAP -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
